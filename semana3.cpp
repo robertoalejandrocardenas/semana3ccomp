077ff0a80cc4145bcec6014840c10eac915e6439
cardenasroberto <101744240+cardenasroberto@users.noreply.github.com>
2022-09-06 08:29:50 -0500

    Add files via upload

---
+++ semana3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int balance{50};
+class Account{
+    public:
+
+};
+
+// #include <iostream>
+//#include <string>
+//using namespace std;
+//int main(){
+  //  int saldo = 50;
+    //int retiro{};
+
+    //cout<<"ELIJA MONTO A RETIRAR: ";cin>>retiro;
+    //int saldorestante= saldo-retiro;
+    //if (retiro >saldo)
+    //cout<<"El monto del retiro excedió el saldo de la cuenta";
+    //else
+    //cout<<"TE QUEDAN: "<<saldorestante;
+
+//};
+ 
